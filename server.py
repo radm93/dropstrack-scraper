40e7122478f56134ecdec02fb5fe8a5073a7ede0
radm93 <rafapointek@gmail.com>
2025-11-28 18:55:07 -0300

    Update server.py

--- server.py
+++ server.py
@@ -1,7 +1,12 @@
 from flask import Flask, jsonify
 from scraper import get_all_cases
+import os
 
 app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return jsonify({"status": "OK", "message": "DropStrack scraper running"})
 
 @app.route("/cases")
 def cases():
@@ -11,9 +16,6 @@ def cases():
         "cases": data
     })
 
-@app.route("/")
-def home():
-    return jsonify({"status": "OK", "message": "DropStrack scraper running"})
-
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000)
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host="0.0.0.0", port=port)
